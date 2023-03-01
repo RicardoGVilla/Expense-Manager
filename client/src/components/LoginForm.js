6871ec429edb5c50f3ff9c5d4bc87b88a5083177
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-03-01 09:47:42 -0500

    adding authentication and setting up front end

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("/users/sign_in", {
+        user: {
+          email: email,
+          password: password,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        // Handle success
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle error
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
