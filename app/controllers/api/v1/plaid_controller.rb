f1ff4f07f6e1fe1b3dd474c86f98dfb2f82d311f
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-03-16 18:49:15 -0400

    fetching all transactions

--- app/controllers/api/v1/plaid_controller.rb
+++ app/controllers/api/v1/plaid_controller.rb
@@ -33,4 +33,43 @@ class Api::V1::PlaidController < ApplicationController
   rescue Plaid::PlaidError => e
     render json: { error: e.pretty_error }
   end
+
+  def get_transactions
+    access_token = params[:access_token]
+    configuration = Plaid::Configuration.new
+    configuration.server_index = Plaid::Configuration::Environment['sandbox']
+    configuration.api_key['PLAID-CLIENT-ID'] = ENV['PLAID_CLIENT_ID']
+    configuration.api_key['PLAID-SECRET'] = ENV['PLAID_SECRET']
+
+    api_client = Plaid::ApiClient.new(configuration)
+    client = Plaid::PlaidApi.new(api_client)
+
+    transactions_get_request = Plaid::TransactionsGetRequest.new
+    transactions_get_request.access_token = access_token
+    transactions_get_request.start_date = (Date.today - 30).strftime('%Y-%m-%d')
+    transactions_get_request.end_date = Date.today.strftime('%Y-%m-%d')
+
+    transaction_response = client.transactions_get(transactions_get_request)
+    transactions = transaction_response.transactions
+
+
+    all_transactions = []
+
+    while transactions.length < transaction_response.total_transactions
+      options_payload = {}
+      options_payload[:offset] = transactions.length
+
+      transactions_get_request = Plaid::TransactionsGetRequest.new
+      transactions_get_request.access_token = access_token
+      transactions_get_request.start_date = "2020-01-01"
+      transactions_get_request.end_date = "2021-01-01"
+      transactions_get_request.options = options_payload
+
+      transaction_response = client.transactions_get(transactions_get_request)
+      transactions += transaction_response.transactions
+    end
+
+    render json: all_transactions.to_json
+  end
+
 end
