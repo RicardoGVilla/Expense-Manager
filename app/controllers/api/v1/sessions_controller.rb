46c8016c498bcc3030105897d655c95df86c77a7
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-03-22 18:23:19 -0400

    add changes to env file

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,59 +2,18 @@ class Api::V1::SessionsController < ApplicationController
   include Devise::Controllers::SignInOut
   require 'plaid'
 
-
-
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    # logger.info "Request parameters: #{params.inspect}"
-    # puts "CSRF token received: #{params[:authenticity_token]}"
-    # request.env["devise.mapping"] = Devise.mappings[:user] # Added this line
     user = User.find_by(email: params[:user][:email])
-    puts Gem.loaded_specs.key?('plaid')
-    puts ENV['PLAID_CLIENT_ID']
-    puts ENV['PLAID_SECRET']
-
-    #authentication logic
 
     if user.valid_password?(params[:user][:password])
       sign_in(:user, user)
-    # Set up Plaid API client
-      configuration = Plaid::Configuration.new
-      configuration.server_index = Plaid::Configuration::Environment["sandbox"]
-      configuration.api_key["PLAID-CLIENT-ID"] =  ENV['PLAID_CLIENT_ID']
-      configuration.api_key["PLAID-SECRET"] = ENV['PLAID_SECRET']
+      link_token_response = plaid_client.link_token_create(link_token_create_request(user.id))
 
-      api_client = Plaid::ApiClient.new(
-        configuration
-      )
-
-      client = Plaid::PlaidApi.new(api_client)
-
-      # Grab the client_user_id by searching for the current user in your database
-      client_user_id = user.id
-
-      # Create the link_token with all of your configurations
-      link_token_create_request = Plaid::LinkTokenCreateRequest.new({
-        :user => { :client_user_id => client_user_id.to_s },
-        :client_name => 'My app',
-        :products => %w[auth transactions],
-        :country_codes => ['CA'],
-        :language => 'en'
-      })
-
-      link_token_response = client.link_token_create(
-        link_token_create_request
-      )
-
-      # Pass the result to your client-side app to initialize Link
-      #  and retrieve a public_token
-      link_token = link_token_response.link_token
-
-      # Return the Link token to the front-end
-      render json: { link_token: link_token_response.link_token, user_id: client_user_id}
+      render json: { link_token: link_token_response.link_token, user_id: user.id }
     else
-      puts "User authentication failed"
+      Rails.logger.error "User authentication failed"
       render json: { success: false }
     end
   end
@@ -62,5 +21,27 @@ class Api::V1::SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     render json: { message: "Successfully signed out" }
+  end
+
+  private
+
+  def link_token_create_request(client_user_id)
+    Plaid::LinkTokenCreateRequest.new(
+      user: { client_user_id: client_user_id.to_s },
+      client_name: 'My app',
+      products: %w[auth transactions],
+      country_codes: ['CA'],
+      language: 'en'
+    )
+  end
+
+  def plaid_client
+    configuration = Plaid::Configuration.new
+    configuration.server_index = Plaid::Configuration::Environment["sandbox"]
+    configuration.api_key["PLAID-CLIENT-ID"] =  ENV['PLAID_CLIENT_ID']
+    configuration.api_key["PLAID-SECRET"] = ENV['PLAID_SECRET']
+
+    api_client = Plaid::ApiClient.new(configuration)
+    Plaid::PlaidApi.new(api_client)
   end
 end
