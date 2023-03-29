2f462e17580261915a090dcfdc24f8764ddf4d95
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-03-29 13:35:59 -0400

    adding total expenses to transactions table

--- client/src/components/BankTransactions.js
+++ client/src/components/BankTransactions.js
@@ -21,6 +21,24 @@ const BankTransactions = ({ accessToken }) => {
       });
   }, [accessToken]);
 
+  const expensesByCategory = transactions.reduce((accumulator, transaction) => {
+    const category = transaction.category[0]; // Only consider the first category
+    const amount = Math.abs(transaction.amount);
+
+    if (!accumulator[category]) {
+      accumulator[category] = amount;
+    } else {
+      accumulator[category] += amount;
+    }
+
+    return accumulator;
+  }, {});
+
+  const totalExpenses = Object.values(expensesByCategory).reduce(
+    (total, expense) => total + expense,
+    0
+  );
+
   const showExpenses = () => {
     setShowExpensesByCategory(true);
   };
@@ -28,28 +46,6 @@ const BankTransactions = ({ accessToken }) => {
   const goBack = () => {
     setShowExpensesByCategory(false);
   };
-
-  const expensesByCategory = (() => {
-    const accumulator = {};
-    const categorizedTransactions = [];
-
-    transactions.forEach((transaction) => {
-      const category = transaction.category[0]; // Only consider the first category
-
-      if (!accumulator[category]) {
-        if (!categorizedTransactions.includes(transaction.transaction_id)) {
-          accumulator[category] = Math.abs(transaction.amount);
-          categorizedTransactions.push(transaction.transaction_id);
-        }
-      } else {
-        if (!categorizedTransactions.includes(transaction.transaction_id)) {
-          accumulator[category] += Math.abs(transaction.amount);
-          categorizedTransactions.push(transaction.transaction_id);
-        }
-      }
-    });
-    return accumulator;
-  })();
 
   return (
     <div>
@@ -60,26 +56,29 @@ const BankTransactions = ({ accessToken }) => {
           {isLoading ? (
             <p>Loading Your Bank Transactions...</p>
           ) : transactions && transactions.length > 0 ? (
-            <table>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Description</th>
-                  <th>Amount</th>
-                  <th>Category</th>
-                </tr>
-              </thead>
-              <tbody>
-                {transactions.map((transaction) => (
-                  <tr key={transaction.transaction_id}>
-                    <td>{transaction.date}</td>
-                    <td>{transaction.name}</td>
-                    <td>{Math.abs(transaction.amount)}</td>
-                    <td>{transaction.category[0]}</td>
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Category</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {transactions.map((transaction) => (
+                    <tr key={transaction.transaction_id}>
+                      <td>{transaction.date}</td>
+                      <td>{transaction.name}</td>
+                      <td>{Math.abs(transaction.amount)}</td>
+                      <td>{transaction.category[0]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <p>Total Expenses: {totalExpenses.toFixed(2)}</p>
+            </>
           ) : (
             <p>No Transactions Found...</p>
           )}
@@ -90,22 +89,26 @@ const BankTransactions = ({ accessToken }) => {
           <h2 className="title-transactions">Expenses by Category</h2>
           <button onClick={goBack}>Back to Transactions</button>
           {Object.entries(expensesByCategory).length > 0 ? (
-            <table>
-              <thead>
-                <tr>
-                  <th>Category</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.entries(expensesByCategory).map(([category, total]) => (
-                  <tr key={category}>
-                    <td>{category}</td>
-                    <td>{total.toFixed(2)}</td>
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Category</th>
+                    <th>Total</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {Object.entries(expensesByCategory).map(
+                    ([category, total]) => (
+                      <tr key={category}>
+                        <td>{category}</td>
+                        <td>{total.toFixed(2)}</td>
+                      </tr>
+                    )
+                  )}
+                </tbody>
+              </table>
+            </>
           ) : (
             <p>No Expenses by Category Found...</p>
           )}
